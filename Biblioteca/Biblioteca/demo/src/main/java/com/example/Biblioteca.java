2a953b60066212fe644da2d1cbddccbc7ff32423
Riyu <mfranciscof01@iesalbarregas.es>
2026-02-05 21:29:48 +0100

    Update Biblioteca.java

--- Biblioteca/Biblioteca/demo/src/main/java/com/example/Biblioteca.java
+++ Biblioteca/Biblioteca/demo/src/main/java/com/example/Biblioteca.java
@@ -13,23 +13,5 @@
 package com.example;
 public class Biblioteca {
     
-    // 1. Atributo de la clase inicializado en 0
-    private Usuario[] listaUsuarios = new Usuario[0];
-
-    // 2. Método de creación de usuarios
-    public void registrarUsuario(String nombre, String pass, String rol) {
-        // DEFINIR UNO NUEVO CON LONGITUD +1
-        Usuario[] nuevoArray = new Usuario[this.listaUsuarios.length + 1];
-
-        // Copiar los datos del anterior al nuevo
-        for (int i = 0; i < this.listaUsuarios.length; i++) {
-            nuevoArray[i] = this.listaUsuarios[i];
-        }
-
-        // Añadir el nuevo usuario en la última posición
-        nuevoArray[nuevoArray.length - 1] = new Usuario(nombre, pass, rol);
-
-        // SE LA PONEMOS AL ATRIBUTO DE LA CLASE
-        this.listaUsuarios = nuevoArray;
-    }
 }
+
