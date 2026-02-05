dac2f3e8361c8c82864f49b096911ae595f5eeec
Riyu <mfranciscof01@iesalbarregas.es>
2026-02-05 21:30:06 +0100

    Update Main.java

--- Biblioteca/Biblioteca/demo/src/main/java/com/example/Main.java
+++ Biblioteca/Biblioteca/demo/src/main/java/com/example/Main.java
@@ -1,12 +1,5 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
-        Biblioteca biblio = new Biblioteca();
 
-        biblio.registrarUsuario("Admin01", "root123", "admin");
-        biblio.registrarUsuario("JuanPerez", "password99", "usuario");
-        biblio.registrarUsuario("MariaG", "clave456", "usuario");
-
-    }
 }
