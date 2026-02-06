de9b2f1ce9e8adea3a93aaec67fa3abdc3471551
despejop01-code <despejop01@iesalbarregas.es>
2026-02-06 17:04:10 +0100

    Libro

--- Biblioteca/Biblioteca/demo/src/main/java/com/example/Libro.java
+++ Biblioteca/Biblioteca/demo/src/main/java/com/example/Libro.java
@@ -11,6 +11,77 @@
  * - Método toString para visualización.
  ******************************************************************/
 package com.example;
-public class Libro {
-    
+enum Genero{
+    Romance, manga, ciencia_ficcion, magia, historicos, cientificos, educativos, terror, drama
 }
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private long Isbn;
+    private int n_paginas;
+    private Genero genero;
+
+    public Libro(){
+    }
+
+    public Libro(String titulo, String autor, String editorial, long Isbn, int n_paginas, Genero genero){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.editorial = editorial;
+        this.Isbn = Isbn;
+        this.n_paginas = n_paginas;
+        this.genero = genero;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public long getIsbn() {
+        return Isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        Isbn = isbn;
+    }
+
+    public int getN_paginas() {
+        return n_paginas;
+    }
+
+    public void setN_paginas(int n_paginas) {
+        this.n_paginas = n_paginas;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+}
+    
+
