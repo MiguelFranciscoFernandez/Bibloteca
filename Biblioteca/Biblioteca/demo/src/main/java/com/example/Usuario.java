f8425f73dec68dd64683abeee74bd52deda2ad04
Riyu <mfranciscof01@iesalbarregas.es>
2026-02-06 17:20:47 +0100

    Cambio

--- Biblioteca/Biblioteca/demo/src/main/java/com/example/Usuario.java
+++ Biblioteca/Biblioteca/demo/src/main/java/com/example/Usuario.java
@@ -12,5 +12,31 @@
  ******************************************************************/
 package com.example;
 public class Usuario {
-    
+    private String nombre;
+    private String password;
+    private String rol;
+    private int prestamosActivos; // Para las estadicsticas globales
+
+    public Usuario(String nombre, String password, String rol) {
+        this.nombre = nombre;
+        this.password = password;
+        this.rol = rol;
+        this.prestamosActivos = 0; // Inicialmente sin préstamos
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public String getRol() {
+        return rol;
+    }
+    public int getPrestamosActivos() {
+        return prestamosActivos;
+    }
 }
+
+
